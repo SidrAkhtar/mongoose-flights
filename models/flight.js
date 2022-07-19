d0a25393fac8c50a9e278ec477c4f3bd9e5d7ba8
sidrakhtar <reachsidraakhtar@gmail.com>
2022-07-19 00:12:16 -0700

    create database module & Flight Model

---
+++ models/flight.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+   airline: {
+      type: String,
+      enum: ['American', 'Southwest', 'United']
+   },
+   airport: {
+      type: {String, default: 'DEN'};
+      enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+   },
+   flightNo: {
+      type: Number,
+      min: 10,
+      max: 9999,
+   },
+   departs: {
+      type: Date,
+      default: function() {
+         return new Date().getFullYear();
+      }
+   }
+})
+
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Flight', flightSchema);
